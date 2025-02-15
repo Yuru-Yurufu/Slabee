8d6f6fc6296aa33ff8266d8b5cce80abe745f3c4
Yuru-Yurufu <ayacrabuilder78@gmail.com>
2025-02-15 21:39:39 +0900

    :fire: いらなくなったrender系のクラスを削除 #1

--- src/client/java/com/forestotzka/yurufu/slabee/render/ModRenderPhase.java
+++
@@ -1,8 +0,0 @@
-package com.forestotzka.yurufu.slabee.render;
-
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.RenderPhase;
-
-public abstract class ModRenderPhase {
-    public static final RenderPhase.ShaderProgram SLABEE_TRANSLUCENT_PROGRAM = new RenderPhase.ShaderProgram(GameRenderer::getRenderTypeTranslucentProgram);
-}
